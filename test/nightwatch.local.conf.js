e305b55db8421b088f555dfab5de10970c54c475
Bo Chen <bochen2014@yahoo.com>
2021-03-19 22:29:36 +1100

    update

--- test/nightwatch.local.conf.js
+++ test/nightwatch.local.conf.js
@@ -3,36 +3,47 @@ const SELENIUM_HOST = process.env.SELENIUM_LOCAL_HOST || "127.0.0.1";
 const SELENIUM_PORT = process.env.SELENIUM_LOCAL_PORT || 4444; // 4444 doesn't work on macbook
 
 module.exports = {
-  output_folder: "test/reports",
-  live_output: false,
-  disable_colors: false,
-  selenium: {
-    start_process: false,
-    host: SELENIUM_HOST,
-    port: SELENIUM_PORT,
-  },
+  src_folders: [],
+
   test_settings: {
     default: {
-      // must be accessible from docker container
-      // ifconfig to work out your local ip address for docker container (default bridge)
       launch_url: process.env.SELENIUM_LAUNCH_URL,
-      selenium_host: SELENIUM_HOST,
-      selenium_port: SELENIUM_PORT,
+    },
+
+    selenium: {
+      // Selenium Server is running locally and is managed by Nightwatch
+      selenium: {
+        start_process: false,
+        port: 4444,
+      },
+      webdriver: {
+        start_process: false,
+      },
+    },
+
+    chrome: {
+      extends: "selenium",
       desiredCapabilities: {
         browserName: "chrome",
         chromeOptions: {
+          // https://github.com/nightwatchjs/nightwatch/issues/2617
           w3c: false,
           args: ["--start-maximized"],
         },
-        javascriptEnabled: true,
-        acceptSslCerts: true,
       },
     },
+
     firefox: {
+      extends: "selenium",
       desiredCapabilities: {
         browserName: "firefox",
-        javascriptEnabled: true,
-        marionette: true,
+      },
+    },
+
+    ie: {
+      extends: "selenium",
+      desiredCapabilities: {
+        browserName: "internet explorer",
       },
     },
   },
